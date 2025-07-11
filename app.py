5a04b2d4271817125dbbcf95a7cad7a52f331638
penguinpharmacy <penguinshe@gmail.com>
2025-07-12 03:11:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,8 +83,8 @@ def edit_employee(emp_id):
     return render_template('edit_employee.html', emp=r)
 
 if __name__ == '__main__':
-    if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
 
